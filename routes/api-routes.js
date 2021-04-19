f9d446750c5666f5402e95f96d1286a6fe6783ae
Greyson Mainor <gbmainor98@outlook.com>
2021-04-19 19:25:27 -0400

    added post, and put routes to my api routes folder.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,14 +6,8 @@ require("mongoose");
 
 
 module.exports = (app) => {
-
-    app.get("/api/workouts/range", (req, res) => {
-        db.Workout.find({}).limit(7).then(data => res.json(data))
-            .catch(err => {
-                console.log("error", err);
-                res.json(err);
-            });
-
+    app.get("*", (req, res) => {
+        res.redirect("/");
     });
 
     app.get("/api/workouts", (req, res) => {
@@ -24,7 +18,31 @@ module.exports = (app) => {
             });
     });
 
-    app.get("*", (req, res) => {
-        res.redirect("/");
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).limit(7).then(data => res.json(data))
+            .catch(err => {
+                console.log("error", err);
+                res.json(err);
+            });
+
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({}).then(data => res.json(data))
+            .catch(err => {
+                console.log("error", err);
+                res.json(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id,
+            { $push: { exercises: req.body } },
+            { new: true, runValidators: true })
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("error", err);
+                res.json(err);
+            });
     });
 };
